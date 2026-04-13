2700fa10a9e3e09a0fbad3fd8a442c962c91821e
RafaMullerr <rmuller>
2026-04-13 12:00:29 -0300

    header y footer

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './components/Home/Home'; 
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './screens/Home/Home'; 
